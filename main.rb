4260520ac298dfcbd0a636de2e155c98a1b19961
z0ff <ex1999@msn.com>
2021-04-14 14:46:17 +0900

    test event

--- main.rb
+++ main.rb
@@ -1,33 +1,8 @@
-# URLにアクセスするためのライブラリの読み込み
 require 'open-uri'
-# Nokogiriライブラリの読み込み
 require 'nokogiri'
+require 'readline'
 
-# スクレイピング先のURL
-url = 'https://kamigame.jp/umamusume/page/110667391372886023.html'
-
-charset = nil
-html = URI.open(url) do |f|
-  charset = f.charset # 文字種別を取得
-  f.read # htmlを読み込んで変数htmlに渡す
-end
-
-# htmlをパース(解析)してオブジェクトを生成
-doc = Nokogiri::HTML.parse(html, nil, charset)
-
-# タイトルを表示
-p doc.title
-
-#p doc.css('span.icon_giant').inner_text
-#p doc.css('span.icon_giant').css('a')[:href]
-
-"""
-umamusumes = doc.search('.icon_giant').map{ |node| node.inner_text.strip}
-p umamusumes.length
-
-urls = doc.search('.icon_giant').css('a').map{ |node| node[:href]}
-p urls.length
-"""
+doc = Nokogiri::HTML(URI.open('https://kamigame.jp/umamusume/page/110667391372886023.html'))
 
 umamusumes = []
 urls = []
@@ -36,6 +11,19 @@ for i in 2..6 do
   urls += doc.search("/html/body/div[2]/div[2]/div[1]/main/kamigame-article-body/article/table[#{i}]/tbody/tr/td[1]/span/a").map{ |node| node[:href]}
 end
 
-p umamusumes
-p urls
+#ary = [umamusumes, urls].transpose
+#links = Hash[*ary.flatten]
+#p links
 
+begin
+  name = Readline.readline('育成ウマ娘の名前を入力してください． > ')
+  doc2 = Nokogiri::HTML(URI.open("https://kamigame.jp" + urls[umamusumes.find_index{|s| s == name}]))
+rescue TypeError
+  puts "そのウマ娘は実装されていないか存在しません．"
+  retry
+end
+
+
+p doc2.title
+
+pp doc2.search("#オグリキャップのイベント選択肢と結果--託された想い").inner_text.strip
